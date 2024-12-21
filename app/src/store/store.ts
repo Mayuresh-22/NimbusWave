e2b7aaad0d05d126f6712c0f8f09a0c547bdd4b1
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2024-12-21 17:17:36 +0530

    Add projectReducer to redux store

--- app/src/store/store.ts
+++ app/src/store/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer, { UserState } from "./userSlice";
+import projectReducer from "./projectSlice";
+import { ProjectState } from "./projectSlice";
 
 export interface RootState {
   user: UserState;
+  project: ProjectState;
 }
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    project: projectReducer,
   },
 });
 
