7e9039d0174559a511ea7342989976abe846b608
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2024-12-21 17:17:13 +0530

    Create project slice to store project detaisl of the user

---
+++ app/src/store/projectSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface Project {
+  projectID: string;
+  chatId: string;
+}
+
+export interface ProjectState {
+  project: Project | null;
+  projects: Project[];
+}
+
+const initialState: ProjectState = {
+  project: null,
+  projects: []
+};
+
+const projectSlice = createSlice({
+  name: "project",
+  initialState,
+  reducers: {
+    setProject(state, action: {payload: Project, type: string}) {
+      state.project = action.payload;
+    },
+    addProject(state, action: {payload: Project, type: string}) {
+      state.projects = [...state.projects, action.payload]
+    },
+  },
+});
+
+export const { setProject, addProject } = projectSlice.actions;
+export default projectSlice.reducer;
