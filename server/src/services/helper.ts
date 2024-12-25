8d4135a6d75cc6feebf5654c8f9da174d79058b0
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2024-12-25 17:33:55 +0530

    Add helper functions

---
+++ server/src/services/helper.ts
@@ -0,0 +1,30 @@
+import moment from "moment";
+import { v4 } from "uuid";
+
+/**
+ * Get the current SQL DateTime
+ * @returns SQL DateTime string
+ */
+export const getSQLDateTimeNow = () => {
+  return moment().format("YYYY-MM-DD HH:mm:ss");
+}
+
+/**
+ * Sanitize a string by replacing all non-alphanumeric characters with underscores
+ * @param str 
+ * @returns 
+ */
+export const sanitizeString = (str: string) => {
+  return str.replace(/[^a-zA-Z0-9]/g, "_");
+}
+
+/**
+ * Normalize a project name by replacing all non-alphanumeric characters with hyphens
+ * and appending a UUID and adding a mid string if provided
+ * @param str 
+ * @param mid 
+ * @returns 
+ */
+export const normalizeProjectName = (str: string, mid?: string) => {
+  return str.replace(/[^a-zA-Z0-9]/g, "-").toLowerCase() + `${mid ? "-" + mid : ''}-` + v4().split("-")[0];
+}
