a0882efe642cbfa12045a1a7536b09c16a9dce18
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2024-12-25 17:33:01 +0530

    Implement public route /app from where deployed projects can be accessed, Implement credit request middleware to check user credits before routing the request

---
+++ server/src/api/serve_app/index.ts
@@ -0,0 +1,37 @@
+// this is the entry point for the AI module
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { Bindings } from "../..";
+import { DEPLOYMENT_NOT_FOUND_HTML } from "../../html/deployment_not_found";
+
+const AppEndpointReqSchema = z.object({
+  email: z.string().nonempty().email(),
+  uid: z.string().nonempty(),
+  meta_data: z.object({
+    name: z.string().nonempty().optional(),
+  }),
+});
+
+const AppEndpoint = new Hono<{ Bindings: Bindings }>();
+
+AppEndpoint.get(
+  "/app/:app_name/*",
+  async (c) => {
+    const appName = c.req.param("app_name");
+
+    const projectResult = await c.env.DB.prepare(
+      "SELECT entry_file_path FROM projects WHERE project_app_name = ?"
+    )
+      .bind(appName)
+      .first();
+
+    if (!projectResult) {
+      return c.html(DEPLOYMENT_NOT_FOUND_HTML);
+    }
+    const appHTML =  await fetch(projectResult.entry_file_path as string).then((res) => res.text());
+    return c.html(appHTML);
+  },
+);
+
+export default AppEndpoint;
