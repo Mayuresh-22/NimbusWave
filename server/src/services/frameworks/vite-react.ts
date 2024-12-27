c999a0f045cfa2a8578b98f81f812f25d5becd14
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2024-12-27 16:56:33 +0530

    Comment few debug statements

--- server/src/services/frameworks/vite-react.ts
+++ server/src/services/frameworks/vite-react.ts
@@ -5,19 +5,19 @@ class ViteReactProcessor {
     pathStartWithSlash: boolean = true,
   ): string {
     let processedHTML = indexHTMLFileBuffer;
-    console.log(
-      "------------------------- Processing Vite React HTML file... ---------------------------------",
-    );
-    console.log(processedHTML + "\n");
+    // console.log(
+    //   "------------------------- Processing Vite React HTML file... ---------------------------------",
+    // );
+    // console.log(processedHTML + "\n");
     console.log("Replacers: " + JSON.stringify(replacers, null, 2));
     for (const [key, value] of Object.entries(replacers)) {
       const regex = new RegExp(`${pathStartWithSlash ? "/" : ""}${key}`, "g");
       processedHTML = processedHTML.replace(regex, value);
     }
-    console.log("Processed HTML: " + processedHTML);
-    console.log(
-      "---------------------------------------------------------------------------------------------",
-    );
+    // console.log("Processed HTML: " + processedHTML);
+    // console.log(
+    //   "---------------------------------------------------------------------------------------------",
+    // );
     return processedHTML;
   }
 }
