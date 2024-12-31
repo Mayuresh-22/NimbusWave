cbab0cb16451b17c816ff0084ae0f6618ac2337a
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2024-12-31 16:35:00 +0530

    ignore react/react-in-jsx-scope warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,15 @@ import baseConfig from '@hono/eslint-config';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   ...baseConfig,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  }
 ];
