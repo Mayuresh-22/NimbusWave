9b82e2067e624fb8db822a124ff8535a4d25e543
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2025-05-14 00:54:40 +0530

    Refactor: Move Navigator component inside a div with inter-font class and update index.css

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -6,6 +6,8 @@ import store from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <Navigator />
+    <div className="inter-font">
+      <Navigator />
+    </div>
   </Provider>,
 );
