136d9dcd2301fb75f55f734eb77a79929566d9a4
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2025-05-16 18:01:07 +0530

    Enhance component and add delete handler

--- app/src/components/common/Dialog.tsx
+++ app/src/components/common/Dialog.tsx
@@ -13,7 +13,7 @@ export default function DeleteConfirmDialog({
 }) {
   return (
     <div className="w-full h-full fixed top-0 left-0 bg-black bg-opacity-60 backdrop-blur-sm z-50">
-      <div className="flex w-96 h-44 items-center justify-center bg-gray-900 border border-gray-800 rounded-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="flex w-96 h-44 items-center justify-center bg-gray-900 border border-gray-600 rounded-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div>
           <div className="w-full flex items-center justify-between px-4">
             <h1 className="text-xl font-bold">{title}</h1>
@@ -29,13 +29,13 @@ export default function DeleteConfirmDialog({
           </div>
           <div className="w-full flex items-center justify-end px-4 space-x-5">
             <button
-              className="text-lg font-semibold px-4 py-1 bg-slate-600 rounded-lg"
+              className="text-base font-semibold px-2 py-1 bg-slate-600 rounded-lg"
               onClick={onClose}
             >
               Cancel
             </button>
             <button
-              className="text-lg font-semibold px-4 py-1 bg-red-500 rounded-lg"
+              className="text-base font-semibold px-2 py-1 bg-red-500 rounded-lg"
               onClick={onOk}
             >
               Yes
